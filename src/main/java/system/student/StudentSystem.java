66c34064d155701412d7de115f6a2250e72ff97d
arkad <mojib.khabireh@gmail.com>
2022-04-04 09:10:38 +0430

    Student System is ready

---
+++ src/main/java/system/student/StudentSystem.java
@@ -0,0 +1,24 @@
+package system.student;
+
+import modul.Course;
+import modul.SectionCourse;
+import modul.Student;
+
+public class StudentSystem {
+    Student student;
+
+    public void selectCourse(Course course) {
+        SectionCourse sectionCourse = new SectionCourse(course.getId()
+                , course.getName()
+                , course.getUnit()
+                , course.getProfessorName()
+                , course.getFactuly()
+                , 0);
+        student.insertCourse(sectionCourse);
+    }
+    public void myInfo(){
+        System.out.println(student);
+    }
+    //delete Course after Added
+
+}
