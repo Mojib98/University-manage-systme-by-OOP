651d60a361c773442f85139e94161fe9e77ee578
arkad <mojib.khabireh@gmail.com>
2022-08-08 13:52:27 +0430

    student can add and remove course

--- src/main/java/modul/Student.java
+++ src/main/java/modul/Student.java
@@ -6,6 +6,7 @@ import modul.enumeration.Faculty;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Setter
 @Getter
@@ -17,7 +18,6 @@ public class Student extends Person {
     private int units;
     private List<SectionCourse> courseList = new ArrayList<>();
     private Integer lastScore;
-
 
 
     public Student(String name, String id, Date date, String nationalCode, String user, String password, Faculty faculty) {
@@ -39,34 +39,59 @@ public class Student extends Person {
 
         // System.out.println("hhh");
     }
-/*
-    @Override
-    public String toString() {
-        return "Student{" +
-                "user='" + user + '\'' +
-                ", password='" + password + '\'' +
-                ", faculty='" + faculty + '\'' +
-                ", units=" + units +
-                ", courseList=" + courseList +
-                ", lastScore=" + lastScore +
-                "} " + super.toString();
+
+    /*
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "user='" + user + '\'' +
+                    ", password='" + password + '\'' +
+                    ", faculty='" + faculty + '\'' +
+                    ", units=" + units +
+                    ", courseList=" + courseList +
+                    ", lastScore=" + lastScore +
+                    "} " + super.toString();
+        }
+
+        public Student(String name, String id, String date, String nationalCode,
+                       String user, String password, String faculty) {
+            super(name, id, date, nationalCode);
+            this.user = user;
+            this.password = password;
+            this.faculty = faculty;
+
+        }
+
+
+
+
+    */
+    public void insertCourse(SectionCourse sectionCourse) {
+        var sumUnit = courseList.stream().map((c) -> c.getCourse().getUnit()).reduce(0, Integer::sum);
+        if (this.lastScore >= 17) {
+            if (units + sectionCourse.getCourse().getUnit() <= 24)
+                courseList.add(sectionCourse);
+        } else if (this.lastScore > 12) {
+            if (units + sectionCourse.getCourse().getUnit() <= 20) {
+                courseList.add(sectionCourse);
+            }
+        } else {
+            if (units + sectionCourse.getCourse().getUnit() <= 14) {
+                courseList.add(sectionCourse);
+            }
+        }
     }
-
-    public Student(String name, String id, String date, String nationalCode,
-                   String user, String password, String faculty) {
-        super(name, id, date, nationalCode);
-        this.user = user;
-        this.password = password;
-        this.faculty = faculty;
-
-    }
-
-
-
-
     public void remove(String name) {
         SectionCourse sectionCourse = null;
-        for (SectionCourse course : courseList) {
+//        Predicate<Course> isHere = item -> item.getName().equals(name);
+        var course = courseList.stream()
+                .filter(c->c.getCourse().getName().equals(name)).findAny();
+//        courseList.removeIf(isHere);
+        if (course.isPresent())
+            courseList.remove(course.get());
+        else
+            throw new RuntimeException("course not find");
+     /*   for (SectionCourse course : courseList) {
             if (course.getName().equals(name)) {
                 sectionCourse = course;
                 break;
@@ -76,18 +101,9 @@ public class Student extends Person {
                 courseList.remove(sectionCourse);
             } else System.out.println("not find");
 
-        }
-    }*/
-public void insertCourse(SectionCourse sectionCourse) {
-   var sumUnit= courseList.stream().map((c)->c.getCourse().getUnit()).reduce(0, Integer::sum);
-    if (this.lastScore >= 17) {
-        if (units + sectionCourse.getUnit() <= 24)
-            courseList.add(sectionCourse);
-    } else if (this.lastScore < 17 & this.lastScore > 14) {
-        if (units + sectionCourse.getUnit() <= 20) {
-            courseList.add(sectionCourse);
-        }
+        }*/
     }
-
-}
+/*    public boolean find() {
+        return value % 2 == 0;
+    }*/
 }
